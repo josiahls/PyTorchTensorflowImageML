e5a532f791f9672dfc430559c3121742559520d8
Josiah Laivins <jokellum@northstate.net>
2019-06-12 18:21:14 -0400

    Added: - Intro to computer vision notebook

--- pytorch_tensorflow_image_ml/test/test_utils.py
+++ pytorch_tensorflow_image_ml/test/test_utils.py
@@ -1,6 +1,7 @@
 import pytest
 from pytorch_tensorflow_image_ml.utils.file_handling import get_absolute_path
-
+import pandas as pd
+import os
 
 def test_absolute_path():
     # Can it find the data directory?
@@ -14,3 +15,11 @@ def test_absolute_path():
     assert found_path.__contains__('mnist'), 'Failed in find the mnist directory.' \
                                              'As a note, this directory is expecting ' \
                                              ' the mnist dataset to exist for testing purposes.'
+
+def test_pandas_dataset_load():
+    absolute_path_to_mnist = get_absolute_path('mnist')
+
+    # The os.path.joint is better than using 'some_path' + '/' + 'filename' because it will determine the separator '/'
+    # as needing to either be '/' or '\'. This is a safer approach to combining paths.
+    df = pd.read_csv(os.path.join(absolute_path_to_mnist, 'mnist_train.csv'), nrows=5)
+    print('here as a place holder')
